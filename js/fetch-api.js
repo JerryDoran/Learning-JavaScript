62feca2164934c3d58960225ec592b6ecace136d
Jerry Doran <Jerry.Doran@pccairfoils.com>
2021-06-30 12:07:43 -0400

    fetch errors

--- js/fetch-api.js
+++ js/fetch-api.js
@@ -1,4 +1,4 @@
-const url = 'https://www.course-api.com/react-tours-project';
+const url = 'https://www.course-api.com/react-tours-projects';
 
 // fetch(url)
 //   .then((response) => response.json())
@@ -10,12 +10,17 @@ const url = 'https://www.course-api.com/react-tours-project';
 const getTours = async () => {
   try {
     const response = await fetch(url);
+    if (!response.ok) {
+      const msg = `There was an error "${response.status} ${response.statusText}"`;
+      throw new Error(msg);
+    }
     const data = await response.json();
-    return data;
-    // console.log(data);
+    // return data;
+    console.log(data);
   } catch (error) {
     console.log(error);
   }
 };
 
-console.log(getTours());
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', getTours);
